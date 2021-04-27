4d930338c9c97d6800e228c07d3c72fa669edc95
EdwinMO2200 <edwin220100@outlook.com>
2021-04-27 11:26:42 -0500

    que el cerdo se mueva

--- Villa Platzi/platzi.js
+++ Villa Platzi/platzi.js
@@ -1,5 +1,16 @@
 var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
+//Lienzo cerdo
+var vc = document.getElementById("cerdoMove");
+var papelCerdo = vc.getContext("2d");
+
+//asignar numero teclas
+var teclas = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT:39
+};
 
 var fondo = {
   url : "tile.png",
@@ -10,16 +21,21 @@ var vaca =
   url: "vaca.png",
   cargaOk : false
 };
+
+//objeto cerdo con su posicion
 var cerdo =
 {
   url: "cerdo.png",
-  cargaOk : false
+  cargaOk : false,
+  x: aleatorio(0, 420),
+  y: aleatorio(0, 420)
 };
 var pollo =
 {
   url: "pollo.png",
   cargaOk : false
 };
+
 fondo.imagen = new Image();
 fondo.imagen.src = fondo.url;
 fondo.imagen.addEventListener("load", cargarFondo);
@@ -28,6 +44,7 @@ vaca.imagen = new Image();
 vaca.imagen.src = vaca.url;
 vaca.imagen.addEventListener ("load", cargarVacas)
 
+//Cargar Elemento Cerdo
 cerdo.imagen = new Image();
 cerdo.imagen.src = cerdo.url;
 cerdo.imagen.addEventListener ("load", cargarCerdos)
@@ -35,6 +52,10 @@ cerdo.imagen.addEventListener ("load", cargarCerdos)
 pollo.imagen = new Image();
 pollo.imagen.src = pollo.url;
 pollo.imagen.addEventListener ("load", cargarPollos)
+
+//Escuchar evento cuando se levante una tecla dentro del rango asignado
+document.addEventListener("keydown", moverTeclado);
+
 
 function cargarFondo()
 {
@@ -46,22 +67,26 @@ function cargarVacas()
   vaca.cargaOk = true;
   dibujar();
 }
+
 function cargarCerdos()
 {
   cerdo.cargaOk = true;
   dibujar();
 }
+
 function cargarPollos()
 {
   pollo.cargaOk = true;
   dibujar();
 }
+
 function dibujar()
 {
   if (fondo.cargaOk)
   {
-papel.drawImage(fondo.imagen, 0, 0);
+    papel.drawImage(fondo.imagen, 0, 0);
   }
+
   if (vaca.cargaOk)
   {
     var cantidad = aleatorio(2, 20);
@@ -70,21 +95,21 @@ papel.drawImage(fondo.imagen, 0, 0);
       var y = aleatorio(0, 6);
       var x = x*80;
       var y = y*80;
-  papel.drawImage(vaca.imagen, x, y);
+      papel.drawImage(vaca.imagen, x, y);
     }
-
   }
+
   if (cerdo.cargaOk)
   {
-    var x = aleatorio(0, 420);
-    var y = aleatorio(0, 420);
-papel.drawImage(cerdo.imagen, x, y);
+    //Si el cerdo esta OK, lo vamos a dibujar en el segundo Lienzo no en el primero
+    papelCerdo.drawImage(cerdo.imagen, cerdo.x, cerdo.y);
   }
+
   if (pollo.cargaOk)
   {
     var x = aleatorio(0, 420);
     var y = aleatorio(0, 420);
-papel.drawImage(pollo.imagen, x, y);
+    papel.drawImage(pollo.imagen, x, y);
   }
 }
 
@@ -94,3 +119,39 @@ function aleatorio(min, maxi)
   resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
   return resultado;
 }
+
+function moverTeclado(evento)
+{
+  var movimiento = 10;
+  switch (evento.keyCode)
+  {
+    case teclas.UP:
+      clear(cerdo.x, cerdo.y);
+      cerdo.y = cerdo.y - movimiento;
+      papelCerdo.drawImage(cerdo.imagen, cerdo.x, cerdo.y);
+      break;
+    case teclas.DOWN:
+      clear(cerdo.x, cerdo.y);
+      cerdo.y = cerdo.y + movimiento;
+      papelCerdo.drawImage(cerdo.imagen, cerdo.x, cerdo.y);
+      break;
+    case teclas.LEFT:
+      clear(cerdo.x, cerdo.y);
+          cerdo.x = cerdo.x - movimiento;
+          papelCerdo.drawImage(cerdo.imagen, cerdo.x, cerdo.y);
+
+      break;
+    case teclas.RIGHT:
+      clear(cerdo.x, cerdo.y);
+    cerdo.x = cerdo.x + movimiento;
+    papelCerdo.drawImage(cerdo.imagen, cerdo.x, cerdo.y);
+      break;
+    default:
+    console.log("otra tecla");
+    break;
+  }
+}
+
+function clear(x, y){
+    papelCerdo.clearRect(x, y, vc.width, vc.height);
+  }
